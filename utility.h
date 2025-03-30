1f4a432a52e3cebdb955ab32f9fa08c2ba10b648
doandung123 <doandung2609@gmail.com>
2025-03-31 05:27:42 +0700

    refactor graphics module

--- utility.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILITY_H
-#define UTILITY_H
-
-#include <SDL.h>
-
-void drawChar(SDL_Renderer* renderer, char c, int x, int y, int r, int g, int b);
-void drawText(SDL_Renderer* renderer, const char* text, int x, int y, int r, int g, int b);
-
-#endif // UTILITY_H
